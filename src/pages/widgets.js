805b4e487bfb8025e80efdc3d1d91229cd3b66ff
Clint Mossman <clintmossman808@gmail.com>
2026-03-19 12:05:17 -0700

    Added content lock import to webdocs

--- src/pages/widgets.js
+++ src/pages/widgets.js
@@ -12,6 +12,7 @@ import FlipCard from "@/components/widgets/FlipCard";
 import HorizontalDisplay from "@/components/widgets/HorizontalDisplay";
 import ImageGallery from "@/components/widgets/ImageGallery";
 import ContentLock from "@/components/widgets/ContentLock";
+import ContentLockQuiz from "@/components/widgets/ContentLockQuiz";
 import MediaContainer from "@/components/widgets/MediaContainer";
 import SideBySide from "@/components/widgets/SideBySide";
 import SocialPost from "@/components/widgets/SocialPost";
@@ -78,6 +79,7 @@ export default function Widgets() {
 					<CardHorizontal />
 					<Columns />
 					<ContentLock />
+					<ContentLockQuiz />
 					<ExpandableBanner />
 					<FlipCard />
 					<HorizontalDisplay />
